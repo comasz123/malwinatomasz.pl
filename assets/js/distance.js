2b9dc1ba5cb6733f5bfd8f0325cad106b25f6808
Tomasz Terlecki <tomasz.terlecki@poczta.onet.pl>
2025-06-09 15:52:47 +0200

    no message

--- assets/js/distance.js
+++ assets/js/distance.js
@@ -9,4 +9,4 @@ const car21_4 = parseInt(localStorage.getItem('carPl21')) || 0;
 console.log('car21_4=' + car21_4);
 const carTotal21 = car21_1 + car21_2 + car21_3 + car21_4;
 localStorage.setItem('carTotal21', carTotal21);
-console.log(carTotal21);
+console.log('2021='+carTotal21);
