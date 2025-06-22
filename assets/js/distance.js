dc57be161643c427ef12dc21cc796a6ffe8596ba
Tomasz Terlecki <tomasz.terlecki@poczta.onet.pl>
2025-06-22 19:24:48 +0200

    no message

--- assets/js/distance.js
+++ assets/js/distance.js
@@ -1,7 +1,13 @@
 
 // 2023
 const car23_1 = parseInt(localStorage.getItem('carUSA23')) || 0;
-const carTotal23= car23_1;
+const car23_2 = parseInt(localStorage.getItem('carHisz23')) || 0;
+
+const carTotal23= car23_1 + car23_2;
+
+const flight23_1 = parseInt(localStorage.getItem('flightUSA23')) || 0;
+const flight23_2 = parseInt(localStorage.getItem('flightHisz23')) || 0;
+const flightTotal23= flight23_1 + flight23_2;
 
 // 2022
 const car22_1 = parseInt(localStorage.getItem('carNv22')) || 0;
@@ -13,12 +19,10 @@ const car22_6 = parseInt(localStorage.getItem('carPie22')) || 0;
 const car22_7 = parseInt(localStorage.getItem('carMon22')) || 0;
 const car22_8 = parseInt(localStorage.getItem('carBdn22')) || 0;
 const carTotal22= car22_1 + car22_2 + car22_3 + car22_4 + car22_5 + car22_6 + car22_7 + car22_8;
-console.log('car='+carTotal22);
 
 const flight22_1 = parseInt(localStorage.getItem('flightZz22')) || 0;
 const flight22_2 = parseInt(localStorage.getItem('flightNv22')) || 0;
 const totalFlight22 = flight22_1 + flight22_2;
-console.log(flight22_2);
 
 // 2021
 const car21_1 = parseInt(localStorage.getItem('carWr21')) || 0;
@@ -28,8 +32,8 @@ const car21_4 = parseInt(localStorage.getItem('carPl21')) || 0;
 const carTotal21 = car21_1 + car21_2 + car21_3 + car21_4;
 
 
-const carTotal=carTotal21+carTotal22+carTotal23;
-const flightTotal=totalFlight22;
+const carTotal = carTotal21 + carTotal22 + carTotal23;
+const flightTotal = totalFlight22 + flightTotal23;
 
 console.log('flight='+flightTotal);
 console.log('car='+carTotal);
