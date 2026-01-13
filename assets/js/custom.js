2ed4228bf7f9422b95b3723e76589d228154900d
Tomasz Terlecki <tomasz.terlecki@poczta.onet.pl>
2026-01-13 12:08:11 +0100

    no message

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,21 @@
-
-    document.addEventListener("scroll", function () {
+document.addEventListener("scroll", function () {
     const header = document.getElementById("site-header");
     if (window.scrollY > 50) {
     header.classList.add("scrolled");
 } else {
     header.classList.remove("scrolled");
 }
+});
+document.addEventListener("DOMContentLoaded", () => {
+        const section = document.body.dataset.section;
+        if (!section) return;
+
+        document
+            .querySelectorAll("#site-header .nav-link")
+            .forEach(link => {
+                if (link.dataset.menu === section) {
+                    link.classList.add("active");
+                }
+            });
 });
 
